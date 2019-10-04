1c3f8876e7650714c862e64069878cb80c951f29
grimmdaniel <grimmdani@inf.elte.hu>
2019-10-04 15:18:43 +0200

    Initial Commit

---
+++ SmartExpensesiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartExpensesiOS
+//
+//  Created by Grimm Dániel on 2019. 10. 04..
+//  Copyright © 2019. com.daniel.grimm.smartexpenses. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

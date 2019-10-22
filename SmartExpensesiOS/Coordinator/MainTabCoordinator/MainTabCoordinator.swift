2a256e8e27401b95ea5a4fc8328a8a16bba0e3a9
Daniel Grimm <grimmdani@inf.elte.hu>
2019-10-22 16:01:49 +0200

    skeleton added for currency change screen

--- SmartExpensesiOS/Coordinator/MainTabCoordinator/MainTabCoordinator.swift
+++ SmartExpensesiOS/Coordinator/MainTabCoordinator/MainTabCoordinator.swift
@@ -23,18 +23,21 @@ class MainTabCoordinator: Coordinator {
         let homeScreenCoordinator = HomeScreenCoordinator()
         let myExpensesCoordinator = MyExpensesCoordinator()
         let socialCoordinator = SocialCoordianator()
+        let currencyRateCoordinator = CurrencyRateCoordinator()
         let profileSettingsCoordinator = ProfileSettingsCoordinator()
         
         tabBarController.viewControllers = [
             homeScreenCoordinator.rootViewController,
             myExpensesCoordinator.rootViewController,
             socialCoordinator.rootViewController,
+            currencyRateCoordinator.rootViewController,
             profileSettingsCoordinator.rootViewController
         ]
         
         childCoordinators.append(homeScreenCoordinator)
         childCoordinators.append(myExpensesCoordinator)
         childCoordinators.append(socialCoordinator)
+        childCoordinators.append(currencyRateCoordinator)
         childCoordinators.append(profileSettingsCoordinator)
     }
     

35d8a664b15a6da58aa7a2e51fb2a4f49f716cf4
Daniel Grimm <grimmdani@inf.elte.hu>
2019-10-24 12:37:19 +0200

    my expense cell created with model

---
+++ SmartExpensesiOS/View/XIB/ExpenseCell/ExpenseCell.swift
@@ -0,0 +1,38 @@
+//
+//  ExpenseCell.swift
+//  SmartExpensesiOS
+//
+//  Created by Grimm Dániel on 24/10/2019.
+//  Copyright © 2019 com.daniel.grimm.smartexpenses. All rights reserved.
+//
+
+import UIKit
+
+class ExpenseCell: UITableViewCell {
+
+    @IBOutlet weak var croppedView: UIView!
+    @IBOutlet weak var expenseNameLabel: UILabel!
+    @IBOutlet weak var expenseCategoryLogo: UIImageView!
+    @IBOutlet weak var expensePlaceLogo: UIImageView!
+    @IBOutlet weak var expensePlaceLabel: UILabel!
+    @IBOutlet weak var expensePriceLabel: UILabel!
+    
+    static let nibName = "ExpenseCell"
+    static let cellName = "ExpenseCell"
+    
+    var currentExpense: Expense! {
+        didSet {
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        initializeUIComponents()
+    }
+    
+    private func initializeUIComponents() {
+        croppedView.layer.cornerRadius = 5.0
+        croppedView.layer.borderColor = ColorTheme.primaryColor.cgColor
+        croppedView.layer.borderWidth = 1.0
+    }
+}
--- SmartExpensesiOS/ViewControllers/MyExpensesViewController/View/MyExpensesViewController.swift
+++ SmartExpensesiOS/ViewControllers/MyExpensesViewController/View/MyExpensesViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MyExpensesViewController: UIViewController, StoryboardAble {
     
+    @IBOutlet weak var myExpensesTableView: UITableView!
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         
@@ -22,5 +24,32 @@ class MyExpensesViewController: UIViewController, StoryboardAble {
         
         navigationController?.navigationBar.barTintColor = ColorTheme.primaryColor
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    
+        let nibObject = UINib.init(nibName: ExpenseCell.nibName, bundle: nil)
+        myExpensesTableView.register(nibObject, forCellReuseIdentifier: ExpenseCell.cellName)
+        
+        myExpensesTableView.delegate = self
+        myExpensesTableView.dataSource = self
+        myExpensesTableView.backgroundColor = .white
+    }
+}
+
+extension MyExpensesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ExpenseCell.cellName) as? ExpenseCell else { return UITableViewCell() }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }

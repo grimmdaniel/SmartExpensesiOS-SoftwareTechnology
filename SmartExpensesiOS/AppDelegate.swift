86db2da5b0b9ff68241344226bba9c892f53c9b9
Daniel Grimm <grimmdani@inf.elte.hu>
2019-10-26 15:57:04 +0200

    minor preparation in Appdelegate

--- SmartExpensesiOS/AppDelegate.swift
+++ SmartExpensesiOS/AppDelegate.swift
@@ -22,23 +22,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(userLoggedIn), name: NSNotification.Name(NotificationConstants.signInNotification), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(userLoggedOut), name: NSNotification.Name(NotificationConstants.logOutNotification), object: nil)
         
+        window = UIWindow(frame: UIScreen.main.bounds)
         userLoggedOut()
+        window?.makeKeyAndVisible()
         return true
     }
     
     @objc func userLoggedIn() {
-        window = UIWindow(frame: UIScreen.main.bounds)
         self.mainTabCoordinator = MainTabCoordinator()
         self.window?.rootViewController = self.mainTabCoordinator.rootViewController
-        window?.makeKeyAndVisible()
         self.mainTabCoordinator.start()
     }
     
     @objc func userLoggedOut() {
-        window = UIWindow(frame: UIScreen.main.bounds)
         self.welcomeCoordiator = WelcomeCoordiator()
         self.window?.rootViewController = self.welcomeCoordiator.rootViewController
-        window?.makeKeyAndVisible()
         self.welcomeCoordiator.start()
     }
 }

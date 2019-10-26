22434ba7b8eb0a62b6d86d7ff0ae9d75cc6a7f31
Daniel Grimm <grimmdani@inf.elte.hu>
2019-10-26 19:30:22 +0200

    home view ui added

---
+++ SmartExpensesiOS/ViewControllers/HomeViewController/Cell/HomeRecommendationsTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeRecommendationsTableViewCell.swift
+//  SmartExpensesiOS
+//
+//  Created by Grimm Dániel on 26/10/2019.
+//  Copyright © 2019 com.daniel.grimm.smartexpenses. All rights reserved.
+//
+
+import UIKit
+
+class HomeRecommendationsTableViewCell: UITableViewCell {
+    
+    static let cellID = "HomeRecommendationsTableViewCell"
+}

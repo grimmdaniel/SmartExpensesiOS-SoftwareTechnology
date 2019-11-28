8c3efc189bb25191a749bc70f2079dd67993baf4
Daniel Grimm <grimmdani@inf.elte.hu>
2019-11-28 11:18:50 +0100

    card pop up added

---
+++ SmartExpensesiOS/ViewControllers/DetailedExpenseViewController/View/SlideUpVC/SlideUpVC.swift
@@ -0,0 +1,22 @@
+//
+//  SlideUpVC.swift
+//  SmartExpensesiOS
+//
+//  Created by Grimm Dániel on 28/11/2019.
+//  Copyright © 2019 com.daniel.grimm.smartexpenses. All rights reserved.
+//
+
+import UIKit
+
+class SlideUpVC: UIViewController {
+
+    @IBOutlet weak var handleView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
